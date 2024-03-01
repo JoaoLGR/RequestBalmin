edd89c7b0180d5e2ff7206d0d32d1f48ca0afb0a
JoaoLGR <joaolucasgomesrocha@gmail.com>
2024-02-29 21:11:30 -0300

    refactor: Melhora a funcionalidade de remover dados do arquivo json

--- RequestBalmin/Controllers/RequestBalminController.cs
+++ RequestBalmin/Controllers/RequestBalminController.cs
@@ -68,12 +68,23 @@ namespace RequestBalmin.Controllers
             if (System.IO.File.Exists($"{filePath}.json"))
             {
                 string jsonContent = System.IO.File.ReadAllText($"{filePath}.json");
-                string dataFile = ReadFileService.RemoveDataOfFile(jsonContent, key, value);
 
-                System.IO.File.WriteAllText($"{filePath}.json", dataFile);
+                try
+                {
+                    JToken.Parse(jsonContent);
 
-                return Content(dataFile, "application/json");
+                    string dataFile = ReadFileService.RemoveDataOfFile(jsonContent, key, value);
+
+                    System.IO.File.WriteAllText($"{filePath}.json", dataFile);
+
+                    return Content(dataFile, "application/json");
+                }
+                catch (JsonReaderException ex)
+                {
+                    return BadRequest($"Erro no processamento do arquivo {fileName}: {ex.Message}");
+                }
             }
+
             return BadRequest("Arquivo não encontrado!");
         }
     }
